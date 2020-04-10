7a8ce16c8e01fa4add8e99901cc8148c2b2f0beb
tikhsuP <pushkit20@gmail.com>
2020-04-10 13:18:37 +0530

    push

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
         <Login />
         <Portfolio/>
         <UserForm />*/}
-        {/* <Profile /> */}
+        <Profile />
  </>
 
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 
-export class Profile extends Component {
+export class Sidebar extends Component {
     render() {
         return (
             <>
-            <div className="sidebar"></div>
-
+                
             </>
         )
     }
 }
 
-export default Profile
+export default Sidebar
