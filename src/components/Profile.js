c64dcc003bf02352230e40a67f529848f084ae46
tikhsuP <pushkit20@gmail.com>
2020-04-10 14:12:18 +0530

    Working

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,7 +4,37 @@ export class Profile extends Component {
     render() {
         return (
             <>
-            <div className="sidebar"></div>
+            <div className="sidebar">
+                <div className="Image">
+                    <img src=""/>
+                </div>
+                <div className="profile">
+
+                </div>
+                <div className="contacts"></div>
+            </div>
+            <div>
+                <div>
+                        <div>
+                                <div className="bio"></div>
+                        </div>
+                        <div>
+
+                        </div>
+                </div>
+                <div className="comments">
+
+                </div>
+                <div className="exp">
+
+                </div>
+                <div className="edu">
+
+                </div>
+                <div className="skills">
+
+                </div>
+            </div>
 
             </>
         )
