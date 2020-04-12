4cfaaa98d928350e45922a6520f353253eee4252
tikhsuP <pushkit20@gmail.com>
2020-04-12 20:44:06 +0530

    addedtimeline

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './css/profile.css';
 import p from '../components/Images/p.jpg';
+import home from '../components/Images/home.jpg';
+
 
 
 export class Profile extends Component {
@@ -9,7 +11,7 @@ export class Profile extends Component {
             <div className="containr">
                     <div className="sidebar">
                         <div className="image">
-                            <img className="image" src= {p} alt=""/>
+                            {/* <img className="image" src= {p} alt=""/>  */}
                         </div>
                         <div className="intro">
                             <p className="txt-h">Name:</p>
@@ -21,7 +23,6 @@ export class Profile extends Component {
                             <p className="txt-h">Phone:</p>
                             <p className="txt">997784 456</p>
                         </div>
-                        
                     </div>
 
 
@@ -36,25 +37,130 @@ export class Profile extends Component {
                                 </div>
                                 </div>
                                 <div className="pict">
-                                <img className="pict" src= {p} alt=""/>                                </div>
+                                <img className="pict" src= {home} alt=""/>                                </div>
                         </div>
                         <div  className="her-cont">
-                            <div className="comments">
-                                <p>3Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </div>
-                            <div className="exp">
-                                <p>4Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </div>
+
                             <div className="edu">
+                                <div className="timeline">
+                                    <ul>
+                                        <li>
+                                            <div className="content"><h1>
+                                                Lorem Ipsum dolor sit amet</h1>
+                                                <p>aezsxdcfvhjbmk</p>
+                                            </div>
+                                            <div className="time">
+                                                nov 2020
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div className="content"><h1>
+                                                Lorem Ipsum dolor sit amet</h1>
+                                                <p>aezsxdcfvhjbmk</p>
+                                            </div>
+                                            <div className="time">
+                                                nov 2020
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div className="content"><h1>
+                                                Lorem Ipsum dolor sit amet</h1>
+                                                <p>aezsxdcfvhjbmk</p>
+                                            </div>
+                                            <div className="time">
+                                                nov 2020
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div className="content"><h1>
+                                                Lorem Ipsum dolor sit amet</h1>
+                                                <p>aezsxdcfvhjbmk</p>
+                                            </div>
+                                            <div className="time">
+                                                nov 2020
+                                            </div>
+                                        </li>
+                                        <div className="stylee"></div>
+                                    </ul>
+                                </div>
+                            </div>
+
+                        <div>
+                            <div className="exp">
+                            <div className="timeline">
+                                    <ul>
+                                        <li>
+                                            <div className="content"><h1>
+                                                Lorem Ipsum dolor sit amet</h1>
+                                                <p>aezsxdcfvhjbmk</p>
+                                            </div>
+                                            <div className="time">
+                                                nov 2020
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div className="content"><h1>
+                                                Lorem Ipsum dolor sit amet</h1>
+                                                <p>aezsxdcfvhjbmk</p>
+                                            </div>
+                                            <div className="time">
+                                                nov 2020
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div className="content"><h1>
+                                                Lorem Ipsum dolor sit amet</h1>
+                                                <p>aezsxdcfvhjbmk</p>
+                                            </div>
+                                            <div className="time">
+                                                nov 2020
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div className="content"><h1>
+                                                Lorem Ipsum dolor sit amet</h1>
+                                                <p>aezsxdcfvhjbmk</p>
+                                            </div>
+                                            <div className="time">
+                                                nov 2020
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div className="content"><h1>
+                                                Lorem Ipsum dolor sit amet</h1>
+                                                <p>aezsxdcfvhjbmk</p>
+                                            </div>
+                                            <div className="time">
+                                                nov 2020
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div className="content"><h1>
+                                                Lorem Ipsum dolor sit amet</h1>
+                                                <p>aezsxdcfvhjbmk</p>
+                                            </div>
+                                            <div className="time">
+                                                nov 2020
+                                            </div>
+                                        </li>
+                                        <div className="stylee"></div>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="comments">
                                 <p>5Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                             </div>
                             <div className="skills">
-                                <p>6Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                <div className="circular-p-bar">
+                                    <div className="con-cpb">
+                                        
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
+            </div>
         )
     }
 }
