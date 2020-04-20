9599af2ae771c3f7d9cdf7086cd162575ec2df51
tikhsuP <pushkit20@gmail.com>
2020-04-20 23:18:53 +0530

    un important

---
+++ src/components/nc.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class nc extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
