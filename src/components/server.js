fdefd163b3939b6801cf951c3727aca27b156fc1
tikhsuP <pushkit20@gmail.com>
2020-04-22 16:42:39 +0530

    moinor

--- src/components/server.js
+++ src/components/server.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export default class server extends Component {
     render() {
         return (
-            <div>
+            <>
                 
-            </div>
+            </>
         )
     }
 }
